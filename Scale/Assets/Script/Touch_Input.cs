95ab344ac656d60dd671a8bc5bac291c23b314ec
ewanmoshi@live.com <ewanmoshi@live.com>
2015-07-23 14:22:00 +1200

    Putting in work

---
+++ Scale/Assets/Script/Touch_Input.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch_Input : MonoBehaviour {
+
+	private Touch[] touches; 
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		//touches = new Touch[10];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		touches = Input.touches;
+
+	}
+
+	void OnGUI(){
+
+		foreach (Touch t in touches){
+
+			GUI.Label(new Rect(t.position.x,t.position.y,100,100),"x coordinate: " + t.position.x + "y coordinate: " + t.position.y );
+
+		}
+
+	}
+}
