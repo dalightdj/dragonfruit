fda71b352f88b793bd868535d4224b39da2433ea
dalightdj <dalightdj@yahoo.com.au>
2015-07-23 15:46:19 +1200

    Add TileInfo script

---
+++ Scale/Assets/Scripts/TileInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileInfo : MonoBehaviour {
+
+	public int population;
+	public int material;
+	public int pollution;
+	public int food;
+}
