f70dd660398e049c8351bb606dd37d22a13bf643
dalightdj <dalightdj@yahoo.com.au>
2015-07-30 16:54:53 +1200

    Add close build menu method

--- Scale/Assets/Scripts/BuildMenuScript.cs
+++ Scale/Assets/Scripts/BuildMenuScript.cs
@@ -16,23 +16,37 @@ public class BuildMenuScript : MonoBehaviour {
 	
 	}
 
+	//Assuming 'pos' is a Vector 2 with (0,0) at the top-left. 'x' increases to the right. 'y' decreases to the bottom.
 	public void callMenu(Direction dir, Vector2 pos){
 		GameObject menu;
-
+		Vector2 newPos;
+		
 		if (dir == Direction.DOWN) {
 			menu = buildMenus[0];
+			//DOWN center is (x-75)
+			newPos = new Vector2(pos.x-75, pos.y);
 		}
 		else if (dir == Direction.LEFT) {
 			menu = buildMenus[1];
+			//LEFT center is (y+75)
+			newPos = new Vector2(pos.x, pos.y+75);
 		}
 		else if (dir == Direction.UP) {
 			menu = buildMenus[2];
+			//UP center is (x+75)
+			newPos = new Vector2(pos.x+75, pos.y);
 		}
 		else{//(dir == Direction.RIGHT) {
 			menu = buildMenus[3];
+			//RIGHT center is (y-75)
+			newPos = new Vector2(pos.x, pos.y-75);
 		}
-
-
+		
+		menu.GetComponent<RectTransform> ().anchoredPosition = newPos;
 		menu.SetActive (true);
+	}
+
+	public void closeMenu(GameObject menu){
+		menu.SetActive (false);
 	}
 }
