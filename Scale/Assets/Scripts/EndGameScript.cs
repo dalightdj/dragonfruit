c719460c1cc00f27d25fcb73a465b2c424df0a61
dalightdj <dalightdj@yahoo.com.au>
2015-07-30 15:21:59 +1200

    Ending scene returns to Opening screen if left too long

---
+++ Scale/Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("ReturnToOpening");
+	}
+
+	IEnumerator ReturnToOpening(){
+		yield return new WaitForSeconds(60);
+		Application.LoadLevel ("Opening");
+	}
+
+	public void StartGame(){
+		Application.LoadLevel ("Game");
+	}
+}
