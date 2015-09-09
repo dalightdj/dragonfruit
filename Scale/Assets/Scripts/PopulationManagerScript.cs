f1f03b84d1ff3a45bd7c455dc75c7f4066fd11ba
dalightdj <dalightdj@yahoo.com.au>
2015-09-10 10:21:44 +1200

    PopulationManager

---
+++ Scale/Assets/Scripts/PopulationManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopulationManagerScript : MonoBehaviour {
+
+	private int maxPopulation;
+	private int currentPopulation;
+	private int employed;//currentPop - employed = unemployed
+	private float populationGrowth;
+	private int foodRequirement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
