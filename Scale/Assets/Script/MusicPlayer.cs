508d4abfe3c61287b4c6c08c0310a7f738270f27
craigthelinguist <aaroncraig@protonmail.ch>
2015-09-24 13:47:00 +1200

    adding sfx capabilities

---
+++ Scale/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip[] music;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		source = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool changeSong(int songIndex){
+
+		source.Pause ();
+		source.clip = music [songIndex];
+		source.Play();
+		return true;
+
+	}
+
+}
---
+++ Scale/Assets/Script/SFXController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXController : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip[] sfx;
+
+
+	// Use this for initialization
+	void Start () {
+
+		source = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void playClip(int clipNumber){
+
+		source.PlayOneShot (sfx[clipNumber]);
+	}
+}
