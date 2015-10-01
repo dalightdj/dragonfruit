0649b969f6768eb9f457a035271512cea456198c
craigthelinguist <aaroncraig@protonmail.ch>
2015-10-01 16:42:54 +1300

    music for final scene

---
+++ Scale/Assets/Script/fadeIn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeIn : MonoBehaviour {
+
+	public Texture2D texture;
+	public float fadeSpeed = 0.1f;
+
+	private int drawDepth = -1000;
+	public float alpha = 1.0f;
+	public int fadeDir = -1;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI(){
+
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.b, GUI.color.g, alpha);
+
+		GUI.depth = drawDepth;
+
+		GUI.DrawTexture (new Rect(0,0,Screen.width, Screen.height), texture);
+	}
+}
