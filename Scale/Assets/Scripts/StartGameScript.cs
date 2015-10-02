e1101ef4674128e1f38c40f77ce195dea69fc741
nealhartley <neal.w.hartley@gmail.com>
2015-10-02 13:27:50 +1300

    fade in to game scene

--- Scale/Assets/Scripts/StartGameScript.cs
+++ Scale/Assets/Scripts/StartGameScript.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class StartGameScript : MonoBehaviour {
 
 	public void StartGame(){
-		Application.LoadLevel ("LoadScene");
+		Application.LoadLevel ("Game");
 	}
 }
