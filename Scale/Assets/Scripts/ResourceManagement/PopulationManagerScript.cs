7ccabe9ef4599986a01e2ebc6078934c66695f36
dalightdj <dalightdj@yahoo.com.au>
2015-10-08 12:33:22 +1300

    Balance population algorithm

--- Scale/Assets/Scripts/ResourceManagement/PopulationManagerScript.cs
+++ Scale/Assets/Scripts/ResourceManagement/PopulationManagerScript.cs
@@ -4,35 +4,41 @@ using System.Collections;
 //A class for managing the population. It inherits from the ResourceManagementScript
 public class PopulationManagerScript : ResourceManagementScript {
 
-	private int maxPopulation = 50;
-	private int currentPopulation = 5;
-	private int employed;//currentPop - employed = unemployed
-	public float foodRequirementPerPerson = 0.0001f;
+	private float maxPopulation = 0;
+	private float currentPopulation = 5;
+	private float employed;//currentPop - employed = unemployed
+	public float foodRequirementPerPerson = 0.001f;
 
 	// Use this for initialization
 	void Start () {
-		GameController.gameController.addResources (currentPopulation, 0, 0, 0);
+		maxPopulation = GameController.gameController.getTotalPopulation ();
+		GameController.gameController.addResources (-(maxPopulation-currentPopulation), 0, 0, 0);
+		increaseGrowthRate (10);
 	}
 	
 
 	// Update is called once per frame
 	void Update () {
 		//Make sure there is enough food
+
 		if (GameController.gameController.sufficientResourses (0, 0, 0, foodRequirementPerPerson * currentPopulation)) {
 			GameController.gameController.addResources (0, 0, 0, -(foodRequirementPerPerson * currentPopulation));
-		} else {
-			GameController.gameController.addResources (-0.85f, 0, 0, 0);
+		} else {//If not enough food then population starts plummeting
+			GameController.gameController.addResources (-0.15f, 0, 0, 0);
 		}
 
 		//Do not exceed max population
 		if (GameController.gameController.getTotalPopulation() < maxPopulation) {
-			currentPopulation++;
+			print ("C:" + currentPopulation);
+			print ("T:" + GameController.gameController.getTotalPopulation());
+			print ("M:" + maxPopulation);
 			base.Update ();//Call the ResourceManagementScript's update method
 		}
 	}
 
 	public override void addResources(){
 		GameController.gameController.addResources (1, 0, 0, 0);
+		currentPopulation++;
 	}
 
 	public void increaseMaxPopulation(int increase){
